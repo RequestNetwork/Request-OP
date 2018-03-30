14b4f3fddb2ca31cd7aec938fe24d10e121c3a0f
Elliott Denis <elliottdenis@hotmail.fr>
2018-03-30 11:42:42 +0800

    update lib version

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { AppRoutingModule } from './app-routing.module';
 
 import { RequestService } from './services/request.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
 import { ConfirmComponent } from './confirm/confirm.component';
 import { HomeComponent } from './home/home.component';
 
+import { RunKitEmbedComponent } from 'angular-runkit'
+
 
 @NgModule({
   declarations: [
+    RunKitEmbedComponent,
     AppComponent,
     ShopComponent,
     ConfirmComponent,
